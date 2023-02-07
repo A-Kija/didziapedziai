ccf7cd5200b38e147f324059e8d4ae7038b5b875
A-Kija <arvydas.kijakauskas@gmail.com>
2023-02-07 12:54:56 +0200

    set

---
+++ 005/app.js
@@ -0,0 +1,11 @@
+console.log('Hello 005');
+
+
+// let color = 'skyblue';
+
+// localStorage.setItem('myFavoriteColor', color);
+
+localStorage.removeItem('myFavoriteColor');
+
+
+console.log(localStorage.getItem('myFavoriteColor'));
