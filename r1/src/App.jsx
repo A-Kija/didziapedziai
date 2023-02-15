41a468641befc4f473a2dd037f6688ef4b5a2e94
A-Kija <arvydas.kijakauskas@gmail.com>
2023-02-15 14:27:35 +0200

    010

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -4,6 +4,12 @@ import './App.scss';
 import AddButton from './Components/010/Buttons/AddButton';
 import ClearButton from './Components/010/Buttons/ClearButton';
 import ColorButton from './Components/010/Buttons/ColorButton';
+import DefaultSortButton from './Components/010/Buttons/DefaultSortButton';
+import Filter500Button from './Components/010/Buttons/Filter500Button';
+import ShowAllButton from './Components/010/Buttons/ShowAllButton';
+import SortButton from './Components/010/Buttons/SortButton';
+import SpinAllButton from './Components/010/Buttons/SpinAllButton';
+import StopSpinButton from './Components/010/Buttons/StopSpinButton';
 import Sq from './Components/010/Sq';
 
 
@@ -18,13 +24,19 @@ function App() {
 
                 <div className="sq-bin">
                     {
-                        sq.map((s, i) => <Sq key={i} s={s} i={i} setSq={setSq} />)
+                        sq.map((s, i) => s.show ? <Sq key={i} s={s} i={i} setSq={setSq} /> : null)
                     }
                 </div>
                 <div className="sq-bin">
                     <AddButton setSq={setSq} classes="blue" />
                     <ClearButton setSq={setSq} classes="red" />
                     <ColorButton setSq={setSq} classes="coral" />
+                    <StopSpinButton setSq={setSq} classes="" />
+                    <SpinAllButton setSq={setSq} classes="" />
+                    <SortButton setSq={setSq} classes="blue" />
+                    <DefaultSortButton setSq={setSq} classes="blue" />
+                    <Filter500Button setSq={setSq} classes="red" />
+                    <ShowAllButton setSq={setSq} classes="red" />
                 </div>
 
             </header>
