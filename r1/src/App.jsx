5ead860ffe277c39b14a30167f7f3e344a17da3e
A-Kija <arvydas.kijakauskas@gmail.com>
2023-02-23 14:23:47 +0200

    create

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from 'react';
 import Create from './Components/Dices/Create';
+import List from './Components/Dices/List';
+import { create, read } from './Components/Dices/localStorage';
 import './Components/Dices/style.scss';
 
+const KEY = 'FancyDices';
 
 function App() {
+
+    const [lastUpdate, setLastUpdate] = useState(Date.now());
+    const [list, setList] = useState([]);
+    const [createData, setCreateData] = useState(null);
+
+    useEffect(() => {
+
+        setList(read(KEY));
+
+    }, [lastUpdate]);
+
+    
+    useEffect(() => {
+        if (null === createData) {
+            return;
+        }
+        create(KEY, createData);
+        setLastUpdate(Date.now());
+    }, [createData]);
 
     return (
         <div className="dices">
             <div className="content">
                 <div className="left">
-                    <Create />
+                    <Create setCreateData={setCreateData}/>
                 </div>
                 <div className="right">
-
+                    <List list={list} />
                 </div>
             </div>
         </div>
