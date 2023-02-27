ea6b0e570bf3c6f47e7c307fb2a8322651637d52
A-Kija <arvydas.kijakauskas@gmail.com>
2023-02-27 14:21:02 +0200

    dices OK ls crud

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import Create from './Components/Dices/Create';
 import List from './Components/Dices/List';
 import { create, destroy, edit, read } from './Components/Dices/localStorage';
+import Messages from './Components/Dices/Messages';
 import './Components/Dices/style.scss';
+import {v4 as uuidv4} from 'uuid';
 
 const KEY = 'FancyDices';
 
@@ -15,12 +17,18 @@ function App() {
     const [deleteData, setDeleteData] = useState(null);
     const [editModal, setEditModal] = useState(null);
     const [editData, setEditData] = useState(null);
+    const [messages, setMessages] = useState(null);
+
+    useEffect(() => {
+        msg('Look at this beautiful DICES', '');
+    }, []);
 
     useEffect(() => {
 
         // setTimeout(() => setList(read(KEY)), 1000);
 
         setList(read(KEY));
+        // msg('Look at this beautiful DICES', '');
 
     }, [lastUpdate]);
 
@@ -31,6 +39,7 @@ function App() {
         }
         create(KEY, createData);
         setLastUpdate(Date.now());
+        msg('Ok, there is new DICE', 'ok');
     }, [createData]);
 
     useEffect(() => {
@@ -39,6 +48,7 @@ function App() {
         }
         destroy(KEY, deleteData.id);
         setLastUpdate(Date.now());
+        msg('The DICE is gone now', 'error');
     }, [deleteData]);
 
     useEffect(() => {
@@ -47,7 +57,16 @@ function App() {
         }
         edit(KEY, editData, editData.id);
         setLastUpdate(Date.now());
+        msg('The DICE is different now', 'ok');
     }, [editData]);
+
+    const msg = (text, type) => {
+        const uuid = uuidv4();
+        setMessages(m => [...m ?? [], {text, type, id: uuid}]);
+        setTimeout(() => {
+            setMessages(m => m.filter(m => uuid !== m.id));
+        }, 5000);
+    } 
 
     return (
         <>
@@ -69,6 +88,9 @@ function App() {
                 </div>
             </div>
         </div>
+        {
+            messages && <Messages messages={messages} />
+        }
         </>
     );
 
