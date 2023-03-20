2eaa000c990c95722b6acbfab20fa393f860e8f3
A-Kija <arvydas.kijakauskas@gmail.com>
2023-03-20 13:15:40 +0200

    sr3 db

---
+++ s3/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3003;
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dp1'
+});
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`LN is on port number: ${port}`);
+});
